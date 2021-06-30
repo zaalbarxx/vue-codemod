5738fbc0ec348ede97d6b30eea0a31b8de9c1071
ygj6 <yuangongji@foxmail.com>
2021-06-30 09:23:17 +0800

    chore: yarn fix

--- transformations/vuex-create-logger.ts
+++ transformations/vuex-create-logger.ts
@@ -2,25 +2,30 @@ import wrap from '../src/wrapAstTransformation'
 import type { ASTTransformation } from '../src/wrapAstTransformation'
 
 export const transformAST: ASTTransformation = ({ root, j }) => {
-//  find the import xxx from 'vuex/dist/logger'
+  //  find the import xxx from 'vuex/dist/logger'
   const importDeclarationCollection = root.find(j.ImportDeclaration, node => {
-    return node.specifiers[0].type === 'ImportDefaultSpecifier'
-      && node.source.value === 'vuex/dist/logger'
+    return (
+      node.specifiers[0].type === 'ImportDefaultSpecifier' &&
+      node.source.value === 'vuex/dist/logger'
+    )
   })
   if (!importDeclarationCollection.length) return
 
-//  remove import
+  //  remove import
   importDeclarationCollection.remove()
 
-//  add import
+  //  add import
   const addImport = require('./add-import')
-  addImport.transformAST({ root, j }, {
-    specifier: {
-      type: 'named',
-      imported: 'createLogger'
-    },
-    source: 'vuex'
-  })
+  addImport.transformAST(
+    { root, j },
+    {
+      specifier: {
+        type: 'named',
+        imported: 'createLogger'
+      },
+      source: 'vuex'
+    }
+  )
 }
 
 export default wrap(transformAST)
