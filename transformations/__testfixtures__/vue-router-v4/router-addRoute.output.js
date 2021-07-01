3898f3667c34499371a215bb0b740b4e39347fa4
star-r <68625641+LiuXing-R@users.noreply.github.com>
2021-07-01 19:18:45 +0800

    fix: avoid failing when repeat -a command (#63)

--- transformations/__testfixtures__/vue-router-v4/router-addRoute.output.js
+++ transformations/__testfixtures__/vue-router-v4/router-addRoute.output.js
@@ -7,11 +7,7 @@ routes.forEach(item => router.addRoute(item))
 
 router.beforeEach((to,from, next) => {
   if(permission){
-    if (Array.isArray(store.getters.addRouters)) {
-      store.getters.addRouters.forEach(item => router.addRoute(item));
-    } else {
-      router.addRoute(store.getters.addRouters);
-    };
+    store.getters.addRouters.forEach(item => router.addRoute(item))
   }
 })
 
--- transformations/router/router-update-addRoute.ts
+++ transformations/router/router-update-addRoute.ts
@@ -3,6 +3,7 @@ import type { ASTTransformation } from '../../src/wrapAstTransformation'
 
 // addRoute() addRoutes()-> forEach addRoute
 export const transformAST: ASTTransformation = context => {
+  
   const { root, j } = context
   const addRouteExpression = root.find(j.CallExpression, {
     callee: {
@@ -14,39 +15,55 @@ export const transformAST: ASTTransformation = context => {
     }
   })
 
-  addRouteExpression.replaceWith(({ node }) => {
-    const routerArgs: any = node.arguments[0]
-    const routerCallee: any = node.callee
+  if (addRouteExpression.length) {
+    addRouteExpression.replaceWith(({ node }) => {
+      // Multiple parameters are not supported
+      const routerArgs: any = node.arguments[0]
+      const routerCallee: any = node.callee
 
-    if (
-      !(
-        routerCallee.object.name === 'router' ||
-        routerCallee.object.name === 'route'
-      )
-    ) {
-      return node
-    }
-
-    const callState = j.callExpression(
-      j.memberExpression(j.identifier('Array'), j.identifier('isArray'), false),
-      [routerArgs]
-    )
-
-    const trueCall = j.callExpression(
-      j.memberExpression(routerArgs, j.identifier('forEach')),
-      [
-        j.arrowFunctionExpression(
-          [j.identifier('item')],
-          j.callExpression(routerCallee, [j.identifier('item')])
+      if (
+        !(
+          routerCallee.object.name === 'router' ||
+          routerCallee.object.name === 'route'
         )
-      ]
-    )
+      ) {
+        return node
+      }
 
-    const trueBlock = j.blockStatement([j.expressionStatement(trueCall)])
-    const falseBlock = j.blockStatement([j.expressionStatement(node)])
+      const arrowFun = root.find(j.ArrowFunctionExpression, {
+        params: [
+          {
+            type: 'Identifier',
+            name: routerArgs.name
+          }
+        ],
+        body: {
+          type: 'CallExpression',
+          callee: {
+            type: 'MemberExpression',
+            property: {
+              type: 'Identifier',
+              name: 'addRoute'
+            }
+          }
+        }
+      })
 
-    return j.ifStatement(callState, trueBlock, falseBlock)
-  })
+      if (arrowFun.length) {
+        return node
+      }
+
+      return j.callExpression(
+        j.memberExpression(routerArgs, j.identifier('forEach')),
+        [
+          j.arrowFunctionExpression(
+            [j.identifier('item')],
+            j.callExpression(routerCallee, [j.identifier('item')])
+          )
+        ]
+      )
+    })
+  }
 
   const addRoutesExpression = root.find(j.CallExpression, {
     callee: {
