ddc118b81972984308826cf185977b2897d33a70
ruleeeer <me@ruleeeer.cn>
2021-07-23 11:50:20 +0800

    feat: implements locale file name lower case (#95)

--- transformations/element-plus-upgrade.ts
+++ transformations/element-plus-upgrade.ts
@@ -2,6 +2,7 @@ import wrap from '../src/wrapAstTransformation'
 import type { ASTTransformation } from '../src/wrapAstTransformation'
 import { transformAST as elementPlusImport } from './element-plus/element-plus-import'
 import { transformAST as elementPlusComponentName } from './element-plus/element-plus-component-name'
+import { transformAST as langLowerCase } from './element-plus/locale-lang-file-lower-case'
 import { getCntFunc } from '../src/report'
 
 export const transformAST: ASTTransformation = context => {
@@ -11,6 +12,7 @@ export const transformAST: ASTTransformation = context => {
   )
   elementPlusImport(context)
   elementPlusComponentName(context)
+  langLowerCase(context)
   const afterCount = Object.keys(subRules).reduce(
     (sum, key) => sum + subRules[key],
     0
