0612a85fde8b1fa485173e77f2d8a4513516a93f
wangsongc <wangsongc@foxmail.com>
2021-07-28 16:53:59 +0800

    feat: add popconfirm-rename-event (#98)

---
+++ vue-transformations/__test__/popconfirm-rename-event.spec.ts
@@ -0,0 +1,9 @@
+import { runTest } from '../../src/testUtils'
+
+runTest(
+  'popconfirm-rename-event',
+  'popconfirm-rename-event',
+  'popconfirm-rename-event',
+  'vue',
+  'vue'
+)
---
+++ vue-transformations/__testfixtures__/popconfirm-rename-event/popconfirm-rename-event.output.vue
@@ -0,0 +1,9 @@
+<template>
+  <el-popconfirm  @confirm="del()"  @cancel="del()" :onConfirm="tt" onCancel="tt"
+  title="Are you sure to delete this?"
+>
+<template #reference>
+  <el-button>Delete</el-button>
+  </template>
+</el-popconfirm>
+</template>
